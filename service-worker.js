5770cc7a7ed24bda8a610a70a11cfde3253e51a3
GabiAharon <146546637+GabiAharon@users.noreply.github.com>
2024-09-15 13:22:11 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,33 @@
-const CACHE_NAME = 'shinun-velemida-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  'https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&display=swap',
-  'https://i.postimg.cc/1z4wp5xG/image.png',
-  'https://www.soundjay.com/human/applause-01.mp3'
-];
+    const CACHE_NAME = 'shinun-velemida-v1';
+    const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192x192.png',
+    './icon-512x512.png',
+    'https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&display=swap',
+    'https://i.postimg.cc/1z4wp5xG/image.png',
+    'https://www.soundjay.com/human/applause-01.mp3'
+    ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+    self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
+    self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+            return response;
+            }
+            return fetch(event.request);
         }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+        )
+    );
+    });
